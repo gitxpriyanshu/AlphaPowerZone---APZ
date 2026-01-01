19d60586e909c7d034f11f2d3fc4bb08d2504e8d
Priyanshu Verma <work.priyanshuverma.1310@gmail.com>
2026-01-02 00:58:57 +0530

    Connect frontend to production backend on Render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3006/api',
+    baseURL: 'https://alphapowerzone-apz.onrender.com/api',
     withCredentials: true, // Important for cookies
 });
 
