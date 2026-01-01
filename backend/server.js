8fcceb0ee273848d42af664deff83c25d7edbc60
Priyanshu Verma <work.priyanshuverma.1310@gmail.com>
2026-01-02 01:20:36 +0530

    Bump version to 1.0.6-live

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('X-App-Version', '1.0.5-final');
+    res.header('X-App-Version', '1.0.6-live');
 
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
